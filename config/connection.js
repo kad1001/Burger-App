15f9c013e1c4468301e6946cac789df8ec95f364
Kelly davis <kellyadavis7@gmail.com>
2019-03-27 23:44:12 -0400

    final i think

--- config/connection.js
+++ config/connection.js
@@ -28,16 +28,3 @@ connection.connect(function(err) {
 
 // Export connection
 module.exports = connection;
-
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
